8e78a5ec89b80fd36746f2e6f8e275d61ec733e2
Hiroki1211 <hiroki1211you@gmail.com>
2023-08-09 19:04:06 +0900

    08-3

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -17,8 +17,13 @@ class Post extends Model
     
     public function getPaginateByLimit(int $limit_count = 5)
     {
-    // updated_atで降順に並べたあと、limitで件数制限をかける
+        // updated_atで降順に並べたあと、limitで件数制限をかける
         return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
+    }
+    
+    public function getSpecific(int $id)
+    {
+        return $this->where('id', $id)->first();
     }
 }
 
